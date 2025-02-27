cab970ac2f6992b0a678907221bd823445203e57
Shivrtn <113790974+Shivrtn@users.noreply.github.com>
2025-02-27 11:55:54 +0530

    Update index.py
    new

--- index.py
+++ index.py
@@ -2,44 +2,46 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd 
 import yfinance as yf
+import os
+
 app = Flask(__name__)
 CORS(app)
+
 @app.route('/post_data', methods=['POST'])
 def post_data():
     # Get the JSON data from the request body
     data = request.get_json()
-    
 
-    sym=(data['symbol'].strip().upper())+'.ns'
-    print(sym)
+    try:
+        sym = (data['symbol'].strip().upper()) + '.NS'
+        print(sym)
 
-    df=yf.download(tickers=sym,start=data['start'],end=data['end'],interval=data['interval'])
-    datetime_series = df.index
-    
+        df = yf.download(tickers=sym, start=data['start'], end=data['end'], interval=data['interval'])
+        datetime_series = df.index
 
-# Convert the datetime series to a datetime format
-    datetime_series = pd.to_datetime(datetime_series)
+        # Convert the datetime series to a datetime format
+        datetime_series = pd.to_datetime(datetime_series)
 
-# Format the datetime values to 'yyyy-mm-dd' format
-    df.index = datetime_series.strftime('%Y-%m-%d')
-    
-    
+        # Format the datetime values to 'yyyy-mm-dd' format
+        df.index = datetime_series.strftime('%Y-%m-%d')
 
+        # Process the data
+        if len(df):
+            df['time'] = df.index.astype(str)
+            df.rename(columns={"Open": "open", "High": "high", "Low": "low", "Close": "close"}, inplace=True)
+            if df.time[0] == df.time[1]:
+                df.time = range(len(df))
+            
+            data_records = df.to_dict(orient='records')
+            response_data = data_records
+        else:
+            response_data = []
 
-    # Process the data (in this example, we'll just echo it back)
-    if len(df):
-        df['time']=df.index.astype(str)
-        df.rename(columns={"Open":"open","High":'high',"Low":"low","Close":'close'},inplace=True)
-        if (df.time[0]==df.time[1]):
-            df.time=range(len(df))
-            #print("yes sir there might be some issue with this code at line two point oo")
-        
-        a=df.to_dict(orient='records')
+        return jsonify(response_data)
 
-
-        rtn=a
-    
-    return jsonify(rtn)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=2000)
+    port = int(os.getenv('PORT', 2000))
+    app.run(host="0.0.0.0", port=port)
